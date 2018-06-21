cee023eb14b9a6db21e36436e18dca15a255297a
Will Chatterson <wjchat@umich.edu>
2018-06-21 16:17:00 -0400

    desktop registration no animation

--- prettyboy/settings.py
+++ prettyboy/settings.py
@@ -28,8 +28,8 @@ DEBUG = True
 ALLOWED_HOSTS = []
 
 EMAIL_HOST = 'smtp.gmail.com'  # since you are using a gmail account
-EMAIL_HOST_USER = 'paulwinegard@gmail.com'
-EMAIL_HOST_PASSWORD = 'Onions11'
+EMAIL_HOST_USER = 'wchatterson@gmail.com'
+EMAIL_HOST_PASSWORD = '125Roman!'
 EMAIL_PORT = 587  # Gmail SMTP port for TLS
 EMAIL_USE_TLS = True
 
